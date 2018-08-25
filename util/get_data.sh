b34b77635579dfdb3b9ca1503abd36efa2a1b4b1
anhv <anhv.ict91@gmail.com>
2018-08-25 21:30:44 +0700

    add get_data util

--- util/get_data.sh
+++ util/get_data.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-wget
+wget https://dl.challenge.zalo.ai/voice/train.zip -P data
+wget https://dl.challenge.zalo.ai/voice/public_test.zip -P data
+wget https://dl.challenge.zalo.ai/voice/sample_submission.csv -P data
