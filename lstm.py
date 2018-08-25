5ca234b9d7401e83c1c6b2fec4c8bfc330e95de8
anhv <anhv.ict91@gmail.com>
2018-08-25 22:22:41 +0700

    update lstm pipeline

--- lstm.py
+++ lstm.py
@@ -14,7 +14,7 @@ from keras.optimizers import Adam, Adadelta, Nadam, Adamax, RMSprop
 from sklearn.model_selection import train_test_split
 from keras import backend as K
 
-print(K.tensorflow_backend._get_available_gpus())
+K.tensorflow_backend._get_available_gpus()
 
 
 def get_model(timeseries, nfeatures, nclass):
@@ -28,38 +28,53 @@ def get_model(timeseries, nfeatures, nclass):
 
 
 data = np.load('./tmp/voice_zaloai/train.npz')
-X, gender, region = data['X'], data['gender'], data['region']
-X_train, X_test, gender_train, gender_test, region_train, region_test = train_test_split(X, gender, region,
+X, gender, accent = data['X'], data['gender'], data['region']
+X_train, X_test, gender_train, gender_test, accent_train, accent_test = train_test_split(X, gender, accent,
                                                                                          test_size=0.2,
                                                                                          random_state=2018)
-gender_region_train = ["{}_{}".format(item[0], item[1]) for item in zip(gender_train, region_train)]
-gender_region_test = ["{}_{}".format(item[0], item[1]) for item in zip(gender_test, region_test)]
 
 publictest = np.load('./tmp/voice_zaloai/publictest.npz')
 X_publictest, fname = publictest['X'], publictest['name']
-
 print('train test: ', X_train.shape, X_test.shape)
 print('public test: ', X_publictest.shape)
 
+
+batch_size = 1024
+nb_epochs = 1000
+
+# Train Gender
+opt = RMSprop()
+model = get_model(X.shape[1], X.shape[2], 2)
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+model.summary()
+print("Train Gender")
+model.fit(X_train, gender_train, batch_size=batch_size, epochs=nb_epochs, validation_data=(X_test, gender_test),
+          verbose=2)
+predicts = model.predict(X_publictest, batch_size=batch_size)
+gender_predicts = np.argmax(predicts, axis=1)
+gender_dict = {0: 'female', 1: 'male'}
+for i in range(32):
+    print(fname[i], '-->', gender_dict[gender_predicts[i]])
+
+# Train Accent
+print("Train Accent")
 opt = RMSprop()
 model = get_model(X.shape[1], X.shape[2], 3)
 model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 model.summary()
-
-batch_size = 1024
-nb_epochs = 10
-
-model.fit(X_train, gender_region_train, batch_size=batch_size, epochs=nb_epochs,
-          validation_data=(X_test, gender_region_test),
+model.fit(X_train, accent_train, batch_size=batch_size, epochs=nb_epochs, validation_data=(X_test, accent_test),
           verbose=2)
-
 predicts = model.predict(X_publictest, batch_size=batch_size)
-predicts = np.argmax(predicts, axis=1)
-
-region_dict = {0: 'north', 1: 'central', 2: 'south'}
-gender_dict = {0: 'female', 1: 'male'}
+accent_predicts = np.argmax(predicts, axis=1)
+accent_dict = {0: 'north', 1: 'central', 2: 'south'}
 for i in range(32):
-    print(fname[i], '-->', region_dict[predicts[i]])
+    print(fname[i], '-->', accent_dict[accent_predicts[i]])
 
-submit = pd.DataFrame.from_dict({'id': fname, 'accent': predicts})
-submit.to_csv('submit.csv', index=False)
+# Results
+df = pd.DataFrame({
+    "id": fname,
+    "gender": gender_predicts,
+    "accent": accent_predicts
+})
+df = df.sort_values("id", ascending=True)
+df.to_csv('tmp/submit.csv', index=False)
